4a0f6ba48767054507007772d2e36a7e387215f4
gamaj <gamajd1@gmail.com>
2019-10-31 21:05:42 -0500

    Semaforo

--- Semaforo.py
+++ Semaforo.py
@@ -6,21 +6,39 @@ import pandas as pd
 import numpy as geek
 
 
+
+
+
 app = dash.Dash()
 
 df = pd.read_csv('Base.csv')
 
 available_indicators = df['Plataforma'].unique()
 
+
+def defaultgraph():
+    lista1 = df['OBJ']
+    lista2 = df['REAL']
+    print (geek.nansum(lista1))
+    print("HOLA...")
+    print(geek.nansum(lista2))
+    print(lista2)
+    piedata = go.Pie(values=[geek.nansum(lista1)-geek.nansum(lista2),geek.nansum(lista2)])
+    
+    return {
+        'data':[piedata],'layout': {'title': 'EJECUCION'}
+    }
+
 app.layout = html.Div([
     html.Div([dcc.Dropdown(
                 id='Actividades',
                 options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Seleccione Actividad'
             )],
         style={'width': '100%', 'display': 'inline-block'}),
     html.Div([ dcc.Graph(
-            id='crossfilter-indicator-scatter'
+            figure = defaultgraph(),
+            id='crossfilter-indicator-scatter',
+            
         )
     ], style={'width': '100%', 'display': 'inline-block', 'padding': '0 20'}),
     ])
@@ -29,17 +47,33 @@ app.layout = html.Div([
     dash.dependencies.Output('crossfilter-indicator-scatter','figure'),
     [dash.dependencies.Input('Actividades','value')])
 def graph(actividad):
-    dff = df[df['Plataforma'] == actividad]
-    lista1 = dff['OBJ']
-    lista2 = dff['REAL']
+    if actividad is None:
+        lista1 = df['OBJ']
+        lista2 = df['REAL']
+        print (geek.nansum(lista1))
+        print(geek.nansum(lista2))
+        print(geek.nansum(lista1)-geek.nansum(lista2))
+        piedata = go.Pie(values=[geek.nansum(lista1)-geek.nansum(lista2),geek.nansum(lista2)])
+    
+        return {
+            'data':[piedata],'layout': {'title': 'EJECUCION'}
+        }
+    else:    
+        dff = df[df['Plataforma'] == actividad]
+        lista1 = dff['OBJ']
+        lista2 = dff['REAL']
+        print(actividad)
+        print (geek.nansum(lista1))
+        print(geek.nansum(lista2))
 
-    piedata = go.Pie(values=[geek.nansum(lista1),geek.nansum(lista2)])
-    print (geek.nansum(lista1))
-    print(geek.nansum(lista2))
-    return {
-        'data':[piedata],'layout': {'title': 'EJECUCION'}
-    }
+        piedata = go.Pie(values=[geek.nansum(lista1)-geek.nansum(lista2),geek.nansum(lista2)])
+        
+        return {
+            'data':[piedata],'layout': {'title': 'EJECUCION'}
+        }
+
+
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
 
