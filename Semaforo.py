58653053dd3b1c166d59de3a32cf968d85865c36
gamaj <gamajd1@gmail.com>
2019-10-31 22:46:35 -0500

    Version con dos graficas más

--- Semaforo.py
+++ Semaforo.py
@@ -16,19 +16,6 @@ df = pd.read_csv('Base.csv')
 available_indicators = df['Plataforma'].unique()
 
 
-def defaultgraph():
-    lista1 = df['OBJ']
-    lista2 = df['REAL']
-    print (geek.nansum(lista1))
-    print("HOLA...")
-    print(geek.nansum(lista2))
-    print(lista2)
-    piedata = go.Pie(values=[geek.nansum(lista1)-geek.nansum(lista2),geek.nansum(lista2)])
-    
-    return {
-        'data':[piedata],'layout': {'title': 'EJECUCION'}
-    }
-
 app.layout = html.Div([
     html.Div([dcc.Dropdown(
                 id='Actividades',
@@ -36,11 +23,17 @@ app.layout = html.Div([
             )],
         style={'width': '100%', 'display': 'inline-block'}),
     html.Div([ dcc.Graph(
-            figure = defaultgraph(),
             id='crossfilter-indicator-scatter',
-            
         )
     ], style={'width': '100%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([ dcc.Graph(
+            id='pieAltipal',
+        )
+    ], style={'width': '100%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([ dcc.Graph(
+            id='barWeek',
+        )
+    ], style={'width': '100%', 'display': 'inline-block', 'padding': '0 20'})
     ])
 
 @app.callback(
@@ -53,8 +46,8 @@ def graph(actividad):
         print (geek.nansum(lista1))
         print(geek.nansum(lista2))
         print(geek.nansum(lista1)-geek.nansum(lista2))
-        piedata = go.Pie(values=[geek.nansum(lista1)-geek.nansum(lista2),geek.nansum(lista2)])
-    
+        piedata = go.Pie(labels= ['Ejecucion' , 'Faltante' ],values=[geek.nansum(lista2),geek.nansum(lista1)-geek.nansum(lista2)])
+        
         return {
             'data':[piedata],'layout': {'title': 'EJECUCION'}
         }
@@ -62,11 +55,61 @@ def graph(actividad):
         dff = df[df['Plataforma'] == actividad]
         lista1 = dff['OBJ']
         lista2 = dff['REAL']
-        print(actividad)
+        print(geek.nansum(lista1)-geek.nansum(lista2))
         print (geek.nansum(lista1))
         print(geek.nansum(lista2))
 
-        piedata = go.Pie(values=[geek.nansum(lista1)-geek.nansum(lista2),geek.nansum(lista2)])
+        piedata = go.Pie(labels= ['Ejecucion' , 'Faltante' ],values=[geek.nansum(lista2),geek.nansum(lista1)-geek.nansum(lista2)])
+        
+        return {
+            'data':[piedata],'layout': {'title': 'EJECUCION'}
+        }
+@app.callback(
+    dash.dependencies.Output('pieAltipal','figure'),
+    [dash.dependencies.Input('Actividades','value')])        
+def graphbar(actividad):
+    if actividad is None:
+        altipal = df[df['Distribuidor']== 'Altipal']
+        dialsa =  df[df['Distribuidor']== 'Dialsa']
+        meico =  df[df['Distribuidor']== 'Meico']
+        piedata = go.Bar(x = ['Altipal' , 'Dialsa','Meico' ],y=[(geek.nansum(altipal['REAL'])/geek.nansum(altipal['OBJ']))*100,(geek.nansum(dialsa['REAL'])/geek.nansum(dialsa['OBJ']))*100,(geek.nansum(meico['REAL'])/geek.nansum(meico['OBJ']))*100])
+        
+        return {
+            'data':[piedata],'layout': {'title': 'EJECUCION'}
+        }
+    else:    
+        dff = df[df['Plataforma'] == actividad]
+        altipal = dff[dff['Distribuidor']== 'Altipal']
+        dialsa =  dff[dff['Distribuidor']== 'Dialsa']
+        meico =  dff[dff['Distribuidor']== 'Meico']
+        piedata = go.Bar(x = ['Altipal' , 'Dialsa','Meico' ],y=[(geek.nansum(altipal['REAL'])/geek.nansum(altipal['OBJ']))*100,(geek.nansum(dialsa['REAL'])/geek.nansum(dialsa['OBJ']))*100,(geek.nansum(meico['REAL'])/geek.nansum(meico['OBJ']))*100])
+        
+        return {
+            'data':[piedata],'layout': {'title': 'EJECUCION'}
+        }
+
+@app.callback(
+    dash.dependencies.Output('barWeek','figure'),
+    [dash.dependencies.Input('Actividades','value')])        
+def graphweek(actividad):
+    if actividad is None:
+        altipal = df[df['Distribuidor']== 'Altipal']
+        dialsa =  df[df['Distribuidor']== 'Dialsa']
+        meico =  df[df['Distribuidor']== 'Meico']
+        piedata = go.Bar(x = ['Altipal' , 'Dialsa','Meico' ],y=[(geek.nansum(altipal['REAL'])/geek.nansum(altipal['OBJ']))*100,(geek.nansum(dialsa['REAL'])/geek.nansum(dialsa['OBJ']))*100,(geek.nansum(meico['REAL'])/geek.nansum(meico['OBJ']))*100])
+        
+        return {
+            'data':[piedata],'layout': {'title': 'EJECUCION'}
+        }
+    else:    
+        dff = df[df['Plataforma'] == actividad]
+        semana = dff
+        semana1 = semana [semana['SEMANA'] == '1']
+        semana2 = semana [semana['SEMANA'] == '2']
+        semana3 = semana [semana['SEMANA'] == '3']
+        semana4 = semana [semana['SEMANA'] == '4']
+        print ((geek.nansum(semana1['REAL'])/geek.nansum(semana1['OBJ']))*100)
+        piedata = go.Scatter(x = ['1','2','3','4'],y=[(geek.nansum(semana1['REAL'])/geek.nansum(semana1['OBJ']))*100,(geek.nansum(semana2['REAL'])/geek.nansum(semana2['OBJ']))*100,(geek.nansum(semana3['REAL'])/geek.nansum(semana3['OBJ']))*100,(geek.nansum(semana4['REAL'])/geek.nansum(semana4['OBJ']))*100])
         
         return {
             'data':[piedata],'layout': {'title': 'EJECUCION'}
